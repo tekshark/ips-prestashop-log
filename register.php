9ff2d43c2afa3176734c3148d71ab702ea22939a
tekshark <38243599+tekshark@users.noreply.github.com>
2018-12-11 14:21:06 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://ipsprestashoplogbot.herokuapp.com/execute.php';
+$BOT_TOKEN = '792644731:AAFNCNnYSvwiVjCiMSyD_3L_V-VKk_AAmf8';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
